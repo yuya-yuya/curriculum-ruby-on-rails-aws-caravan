fc7f7a094b8eb1f42f7a3798297389979bec5a40
yuya-yuya <yytyfp5115fp@icloud.com>
2021-01-04 04:14:18 +0000

    addtional commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,10 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+     @blog = Blog.find(params[:id])
   end
 
   def new
@@ -12,10 +14,25 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    # 以下の行を修正
+    redirect_to blog_path(blog.id) 
   end
 
+
   def edit
+     @blog = Blog.find(params[:id])
+  end
+  
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
   
   private
